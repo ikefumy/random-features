699cfc664fc28c7df11d367f9076e379655f0d29
ikefumy <ikeke0254@gmail.com>
2024-01-01 02:49:22 +0900

    実行しない実験をbashから除外

--- exec.sh
+++ exec.sh
@@ -29,13 +29,13 @@ for i in "${datasets_syn[@]}"; do
     python3 main.py --dataset $i --filename logs/log_GCN_${i} --neighbor_pooling_type average --num_mlp_layers 1
 done
 
-for i in "${mds[@]}"; do
-    python3 dataset_gen.py $i
-    python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --random 100 --filename logs/log_${i}_random
-    python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --filename logs/log_${i}
-    python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --random 100 --filename logs/log_GCN_${i}_random --neighbor_pooling_type average --num_mlp_layers 1
-    python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --filename logs/log_GCN_${i} --neighbor_pooling_type average --num_mlp_layers 1
-done
+# for i in "${mds[@]}"; do
+#     python3 dataset_gen.py $i
+#     python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --random 100 --filename logs/log_${i}_random
+#     python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --filename logs/log_${i}
+#     python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --random 100 --filename logs/log_GCN_${i}_random --neighbor_pooling_type average --num_mlp_layers 1
+#     python3 main.py --dataset $i --hidden_dim 1024 --num_layers 10 --lr 0.1 --opt sgd --epochs 50000 --filename logs/log_GCN_${i} --neighbor_pooling_type average --num_mlp_layers 1
+# done
 
 
 for i in "${datasets_real[@]}"; do
